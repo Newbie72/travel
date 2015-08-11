68ec3a3ee98567930e5d7fc3e652b050f799f5c6
Terence Smith <terencesmith1@gmail.com>
2015-08-10 20:34:00 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = "Valdosta"
+  	@countries = ["Argentina", "Germany", "Korea", "Japan"]
+  	@images = ["argentina.png", "skorea.png", "germany.png"]
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+
+  end
+end
